a1cff5d3d79c49efe4d1138ed6df240103b87c7a
Khrystyna Matasova <>
2022-06-17 17:53:27 +0300

    initial commit with first lesson

---
+++ Concurrency/ConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrencyApp.swift
+//  Concurrency
+//
+//  Created by Khrystyna Matasova on 17/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct ConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
